e8c3ab045487fb214ab14d65c0910b144df56dcb
humonnom <josojoso0702@gmail.com>
2022-06-17 23:40:05 +0900

    Add toEqual, toBeGreaterThan to Jest

---
+++ microJest/index.js
@@ -0,0 +1,30 @@
+const { sum, subtract, sumAsync, subtractAsync } = require("./math");
+const { test, expect } = require("./microJest");
+
+test("sumAsync adds numbers asynchronously", async () => {
+  const result = await sumAsync(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test("subtractAsync subtracts numbers asynchronously", async () => {
+  const result = await subtractAsync(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+test("sum test", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test("sub test", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+test("equal test", () => {
+  expect(1).toBeGreaterThan(2);
+});
--- microJest/microJest.js
+++ microJest/microJest.js
@@ -1,28 +1,3 @@
-const { sum, subtract, sumAsync, subtractAsync } = require("./math");
-
-test("sumAsync adds numbers asynchronously", async () => {
-  const result = await sumAsync(3, 7);
-  const expected = 10;
-  expect(result).toBe(expected);
-});
-
-test("subtractAsync subtracts numbers asynchronously", async () => {
-  const result = await subtractAsync(7, 3);
-  const expected = 4;
-  expect(result).toBe(expected);
-});
-test("sum test", () => {
-  const result = sum(3, 7);
-  const expected = 10;
-  expect(result).toBe(expected);
-});
-
-test("sub test", () => {
-  const result = subtract(7, 3);
-  const expected = 4;
-  expect(result).toBe(expected);
-});
-
 async function test(title, callback) {
   try {
     await callback();
@@ -40,7 +15,17 @@ function expect(actual) {
         throw new Error(`${actual} is not equal to ${expected}`);
       }
     },
-    toEqual() {},
-    toBeGreaterThan() {},
+    toEqual(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toBeGreaterThan(expected) {
+      if (actual <= expected) {
+        throw new Error(`${actual} is not greater than ${expected}`);
+      }
+    },
   };
 }
+
+module.exports = { test, expect };
